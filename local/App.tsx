e6dc6c7ebd449199f31acfbd4e560483a9695282
Marco Maues <marcomauesjr@gmail.com>
2025-02-17 23:10:57 -0300

    Atualização do Ambiente React Native

--- local/App.tsx
+++ local/App.tsx
@@ -15,6 +15,7 @@ import {
   Text,
   useColorScheme,
   View,
+  Button,
 } from 'react-native';
 
 import {
@@ -76,7 +77,7 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
+          <Section title="Primeiro">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
