92777a160f62eb952fac28aab87c1476d716bed1
joao victor <jvictorsbferreira@gmail.com>
2025-02-22 19:40:51 -0300

    Updates

---
+++ _next/static/chunks/d64684d8-1c99d0192a00b89fece0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[556],{3854:function(t,n,r){"use strict";r.d(n,{yoF:function(){return o}});var e=r(4405);function o(t){return(0,e.w_)({tag:"svg",attr:{fill:"none",viewBox:"0 0 24 24",stroke:"currentColor"},child:[{tag:"path",attr:{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M9 5l7 7-7 7"}}]})(t)}}}]);
